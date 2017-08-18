3874eeb069cf5a2da08c85523320b8ed040dbac8
Jeremy Boggs <jeremyboggs@gmail.com>
2017-08-18 12:51:03 -0400

    Change key in filter.

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 
 function filter_contribution_description($components) {
     $components['label'] = "Tell us about this deposit.";
-    $components['description'] = "Please include as much information as you can. For example: What was happening at this moment? Who is involved? What else do we need to know about this moment in time?";
+    $components['comment'] = "Please include as much information as you can. For example: What was happening at this moment? Who is involved? What else do we need to know about this moment in time?";
     return $components;
 }
