bb65aba48287ccf73b6f139a01f0fded0cfdc359
Jeremy Boggs <jeremyboggs@gmail.com>
2017-08-18 12:58:50 -0400

    Removes description.

--- functions.php
+++ functions.php
@@ -2,6 +2,5 @@
 
 function filter_contribution_description($components) {
     $components['label'] = "Tell us about this deposit.";
-    $components['description'] = "Please include as much information as you can. For example: What was happening at this moment? Who is involved? What else do we need to know about this moment in time?";
     return $components;
 }
