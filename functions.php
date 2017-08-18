23f2ef01e281a1fa6b84a1d6ad4af9c5a2c4a145
Jeremy Boggs <jeremyboggs@gmail.com>
2017-08-18 12:46:28 -0400

    Adds filter for DC:Description form field.

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function filter_contribution_description($components) {
+    $components['label'] = "Tell us about this deposit.";
+    $components['description'] = "Please include as much information as you can. For example: What was happening at this moment? Who is involved? What else do we need to know about this moment in time?";
+    return $components;
+}
