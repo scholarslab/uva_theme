f68d2ed736492329796161bcad4e8c8be2aac5f0
Jeremy Boggs <jeremyboggs@gmail.com>
2017-08-18 09:08:28 -0400

    Removes some javascript calls.

--- common/footer.php
+++ common/footer.php
@@ -8,17 +8,4 @@ id="content-info" class="row" role="contentinfo"> <div id="copyright"
 class="container"> <?php if($footer_text = get_theme_option('Footer Text')): ?>
 <p><?php echo nl2br($footer_text); ?></p> <?php endif; ?> </div> </footer>
 </div> </div><!-- /.row /.container --> </div><!-- /#wrap -->
-
-<script
-src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-<script
-src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-
-<script type="text/javascript"
-src="http://use.typekit.com/txp7zqw.js"></script> <script
-type="text/javascript">try{Typekit.load();}catch(e){}</script> <!--[if lt IE 7
-]> <script defer
-src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-<script
-defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-<![endif]--> </body> </html>
+</body> </html>
